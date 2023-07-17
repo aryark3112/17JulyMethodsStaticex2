15a71ebd1ccb7c6db6f3c9dc7652e570f3a9934c
aryark <aryark3112@gmail.com>
2023-07-17 11:44:22 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _17JulyMethods
+{
+    public class OurClass
+    {
+        //instance Mrthod return type void and no parameters
+        public void Display()
+        {
+            Console.WriteLine("Welcome to Methods !!!");
+        }
+        //static method
+        public static void Welcome()
+        {
+            Console.WriteLine("Static Method Welcome you!!!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OurClass obj = new OurClass();
+            obj.Display();
+            OurClass.Welcome();
+            Console.ReadKey();
+        }
+    }
+}
