b97d7ee0140e7b23bae089e50925a50b3d898e84
aryark <aryark3112@gmail.com>
2023-07-17 11:55:23 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -15,14 +15,34 @@ namespace _17JulyMethods
         {
             Console.WriteLine("Static Method Welcome you!!!");
         }
+        //Methods return void & take two parameters
+        public void FullName(string fname, string lname)
+        {
+            Console.WriteLine("Full Name is: " + fname + " " + lname);
+        }
+        //Methods return double & take two parameters
+        public double Add(double n1, double n2)
+        { 
+        double result;
+        result =n1+n2;
+            return result;
+}
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            OurClass obj = new OurClass();
-            obj.Display();
-            OurClass.Welcome();
+            //OurClass obj = new OurClass();
+            //obj.Display();
+
+            //OurClass.Welcome();
+            OurClass ourClass = new OurClass();
+            double num1, num2;
+            Console.WriteLine("Enter first Number");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Result after Addition is: \t"+ourClass.Add(num1,num2));
             Console.ReadKey();
         }
     }
